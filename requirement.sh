ea4f0ba867a9772bac2a585715aeabec9cf57be8
Mehti <34041379+mr-mehti@users.noreply.github.com>
2021-03-12 11:08:25 -1200

    Update requirement.sh

--- requirement.sh
+++ requirement.sh
@@ -1,1 +1,8 @@
-sudo apt-get update && apt install python3-pip -y && pip3 install Flask && nohup python3 -u /root/openvpn/app.py &> /root/openvpn/app.out &
+sudo apt-get update
+
+apt install python3-pip -y 
+
+pip3 install Flask 
+
+nohup python3 -u /root/openvpn/app.py &> /root/openvpn/app.out &
+#1
