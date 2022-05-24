0905843b8917d34f44be63c142fbba8afd6c2596
Mehti <34041379+mr-mehti@users.noreply.github.com>
2022-05-24 15:17:29 -0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,2 +1,3 @@
 cd /root/openvpn && sudo chmod +x openvpn-install.sh && sudo chmod +x requirement.sh && sudo chmod +x ./shadow.sh && sudo chmod +x ./status.sh && sudo ./requirement.sh
 cd /root/openvpn && sudo ./openvpn-install.sh && sudo ./shadow.sh && nohup python3 -u app.py &> ./app.out &
+crontab -l | { cat; echo "crontab -e @reboot cd openvpn && nohup python3 -u app.py &> ./app.out &} | crontab -
